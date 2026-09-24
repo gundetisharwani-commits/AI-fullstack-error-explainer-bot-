9c6d406632c18137d2db98ba375a3edf290ba29f
Gundeti Sharwani <sharwani@gmail.com>
2026-09-24 11:23:54 +0530

    added while loop to repeat

--- mainn.py
+++ mainn.py
@@ -1,6 +1,6 @@
 import ollama
 
-# name = input("Enter your name: ")
+name = input("Enter your name: ")
 # department = input("Enter your department: ")
 # print(f"my name is {name} and I'm from {department} department.")
 system='''
@@ -10,12 +10,27 @@ you explain programming error messages to a 2nd year emgineering student.reply i
 3)how to fix it
 keep it under 120 words.
 '''
-response = ollama.chat(model='gemma3:1b', messages=[
-        {
-            'role': 'system',
-            'content': system
+while True:
+    data=input("enter the error message or exit to stop:").strip()
+    if not data:
+        print("enter an error: ")
+    elif data.lower()=="exit":
+        break
+    elif data.lower()=="help":
+        print("Enter an error message,i find you a solution.")
+    else:
+        try: 
+            response = ollama.chat(model='gemma3:1b', messages=[
+            {
+                'role': 'system',
+                'content': system
 
-        },
-        {'role': 'user', 'content': 'python error-NameError: name"x" is not defined'}
-])
-print(response.message.content)
+            },
+            {
+                'role': 'user', 
+                'content': data
+                }
+        ])
+            print(response.message.content)
+        except Exception as e:
+            print(f"Couldn't reach response:{e}")
